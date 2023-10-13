411c40a93124d8d50a6febb00b390d7821ff499e
Tomás Lopes <83817385+official-zemeister@users.noreply.github.com>
2023-10-13 11:27:47 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,6 +3,16 @@ import h5py as h5
 
 def read_data_map(file):
   """
+  Input:
+        file: string
+            Location of the LIBS map file
+    Returns:
+        spectrum: array_like (x_size, y_size, wavelengths)
+        wavelengths: array_like (num_wavelengths)
+        positions: array_like (2, num_shots)
+        unique_x: array_like (x_size)
+        unique_y: array_like (y_size)
+            Returns the LIBS data cube, along with the spectral wavelengths, the shot positions and the unique positions along each map axis
   """
 
   hf = h5py.File(file + '.h5', 'r')
@@ -15,7 +25,6 @@ def read_data_map(file):
   positions = np.array([np.ndarray.flatten(np.array(list(hf['Sample_ID: ' + sample]['Spot_' + str(i)]['position']))) for i in range(0,
                                                       len(list(hf['Sample_ID: ' + sample])))])
   wavelengths = np.array(hf['System properties']['wavelengths'])
-  # fft_scores = np.array(hf['System properties']['fft_score'])
   
   
   unique_x = np.unique(positions[:,0])
@@ -26,8 +35,7 @@ def read_data_map(file):
   indexes = np.lexsort((positions[:, 0],positions[:, 1]))
   spectrums = spectrums[indexes, :]
   spectrums = np.array(spectrums.reshape((ny, nx, -1)))
-  # print(spectrums.shape)
   positions = np.array(positions[indexes])
   
   hf.close()
-  return spectrums, wavelengths, positions, unique_x, unique_y#, fft_scores
+  return spectrums, wavelengths, positions, unique_x, unique_y
